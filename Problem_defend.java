f5b2bb8b9396ad0871470e4b7431f6bf89d3cb59
Fahima232140 <154117114+Fahima232140@users.noreply.github.com>
2024-04-13 12:47:29 +0600

    Add files via upload

---
+++ Problem_defend.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem_defend {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your sentences: ");
+        String sen = s.nextLine();
+        System.out.print("Enter the size of array: ");
+        int size = s.nextInt();
+        System.out.print("Enter the elements of array: ");
+        int [] arr = new int[size];
+        for (int i = 0; i<size; i++);
+        int array = s.nextInt();
+
+    }
+    public static String find_char(int n){
+
+    }
+    }
